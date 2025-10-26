eaaf03c3b6bbb07264bf1194a6dc5946202d5694
Nikki Pacatang <npacatang89487@liceo.edu.ph>
2025-10-26 12:45:49 +0800

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.trackletics {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.trackletics to javafx.fxml;
+    exports com.example.trackletics;
+}

7841775821aa3ddc0a30c22fe28b4333f6e67609
Nikki Pacatang <npacatang89487@liceo.edu.ph>
2025-10-29 22:43:36 +0800

    login

--- src/main/java/com/example/trackletics/LoginController.java
+++ src/main/java/com/example/trackletics/LoginController.java
@@ -3,7 +3,7 @@ package com.example.trackletics;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class LoginController {
     @FXML
     private Label welcomeText;
 

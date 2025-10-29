29723a2b2e8742b48bb394e696a6a65959498ac9
Nikki Pacatang <npacatang89487@liceo.edu.ph>
2025-10-30 00:04:23 +0800

    image

--- src/main/java/com/example/trackletics/LoginController.java
+++ src/main/java/com/example/trackletics/LoginController.java
@@ -1,14 +1,26 @@
-package com.example.trackletics;
+package com.trackletics;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class LoginController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField emailField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    public void initialize() {
+        // Any setup code
+    }
+
+    @FXML
+    protected void onSignInClicked() {
+        String email = emailField.getText();
+        String password = passwordField.getText();
+        System.out.println("Email: " + email + ", Password: " + password);
     }
 }

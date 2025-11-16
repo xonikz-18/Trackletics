33010515cec261ec6dba4df886302c4be5eb1448
Nikki Pacatang <npacatang89487@liceo.edu.ph>
2025-11-16 14:27:43 +0800

    workout

--- src/main/java/com/example/trackletics/HomeDashboardController.java
+++ src/main/java/com/example/trackletics/HomeDashboardController.java
@@ -1,4 +1,59 @@
 package com.example.trackletics;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import java.io.IOException;
+
 public class HomeDashboardController {
+
+    @FXML
+    private Button quickSleepBtn;
+
+    @FXML
+    private void handleQuickSleepClick () throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("sleepnhydration-view.fxml"));
+        Scene scene = new Scene(loader.load());
+
+        String styleCss = this.getClass().getResource("application.css").toExternalForm();
+        scene.getStylesheets().add(styleCss);
+
+        Stage stage = (Stage) quickSleepBtn.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private Button quickMealBtn;
+
+    @FXML
+    private void handleQuickMealClick () throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("log-meal-view.fxml"));
+        Scene scene = new Scene(loader.load());
+
+        String styleCss = this.getClass().getResource("application.css").toExternalForm();
+        scene.getStylesheets().add(styleCss);
+
+        Stage stage = (Stage) quickMealBtn.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private Button quickWorkoutBtn;
+
+    @FXML
+    private void handleQuickWorkoutClick () throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("workout-view.fxml"));
+        Scene scene = new Scene(loader.load());
+
+        String styleCss = this.getClass().getResource("application.css").toExternalForm();
+        scene.getStylesheets().add(styleCss);
+
+        Stage stage = (Stage) quickWorkoutBtn.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
 }
--- src/main/java/com/example/trackletics/LoginController.java
+++ src/main/java/com/example/trackletics/LoginController.java
@@ -7,7 +7,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.security.PrivateKey;
 
